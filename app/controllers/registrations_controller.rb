f98c741c84dc83e0874ff7ff2c97689a77f855b3
Manuel Fernandez <manfergo25@gmail.com>
2013-09-15 13:12:16 -0430

    Overwrite Devise Registrations Controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,29 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_filter:authenticate_user!, :only => :token
+  
+  def new
+    super
+  end
+  
+  def create
+    @company = Company.new(:name => params[:user][:company_name])
+    @user = User.new(params[:user])
+    User.transaction do
+       @company.save!
+       @user.domain = @company.id
+       @user.save!
+       end
+
+    if @user.save && @company.save
+      flash[:notice] = "You have signed up successfully. If enabled, a confirmation was sent to your email"
+        redirect_to root_url
+    else
+      render :action => :new
+    end
+  end
+  
+  def update
+    super
+  end
+end
+            
