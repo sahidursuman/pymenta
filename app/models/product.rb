bc492ce9f6fc78c9bf6e1808d93c25b8550e6715
manfergo25 <manfergo25@gmail.com>
2013-10-31 08:48:59 -0430

    Unit types select

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,9 @@ class Product < ActiveRecord::Base
   set_primary_key "id"
   include UUIDHelper
   attr_accessible :code, :domain, :description, :id, :price, :units, :username, :version
+
   belongs_to :company, :foreign_key => 'domain'
+
+  UNIT_TYPES = ["UND","MTS","HRS"]
+  validates :units, inclusion: UNIT_TYPES
 end
