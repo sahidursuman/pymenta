6cd653a9a6bf006ab30955e1f6d15c12596b0020
Manuel Fernandez <manfergo25@gmail.com>
2013-11-13 23:38:56 -0430

    Validates Category and Brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,6 +2,10 @@ class Brand < ActiveRecord::Base
   set_primary_key "id"
   include UUIDHelper
   attr_accessible :code, :description, :domain, :id, :username, :version
+  
+  validates :code, presence: true
+  validates :description, presence: true
+  
   belongs_to :company, :foreign_key => 'domain'
   has_many :products, :dependent => :restrict 
 end
