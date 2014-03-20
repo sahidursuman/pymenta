73614480b905c15c348296cd68495cb966ff12d1
Manuel Fernandez <manfergo25@gmail.com>
2014-03-20 17:27:10 +0100

    Change in application.rb

--- config/application.rb
+++ config/application.rb
@@ -83,7 +83,9 @@ module Pymenta
     
    # config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
     config.autoload_paths += %W(#{config.root}/app/models)
-    config.assets.precompile += %w( application.css devise.css *.js )
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.sass.load_paths ||= []
+    config.sass.load_paths << "#{Rails.root}/app/assets/stylesheets"
     
     I18n.default_locale = :es
     I18n.locale = I18n.default_locale
