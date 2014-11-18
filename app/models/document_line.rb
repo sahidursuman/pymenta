34420f0004f62cf9fe103c5860e1b1a360ca39d8
Manu <manfergo25@gmail.com>
2014-11-18 15:47:03 -0430

    Document line sum&substrat and render lines

--- app/models/document_line.rb
+++ app/models/document_line.rb
@@ -9,4 +9,28 @@ class DocumentLine < ActiveRecord::Base
   belongs_to :product
   belongs_to :warehouse
   belongs_to :stock
+
+  before_create :sum_totals
+  before_destroy :substract_totals
+
+  protected
+
+  def sum_totals
+    total = (out_quantity+in_quantity)*price
+    description = product.description
+    code = product.code
+    document.sub_total = document.sub_total + total
+    document.tax_total = document.tax_total + (total*document.tax/100)
+    document.total = document.sub_total + document.tax_total
+    document.paid_left = document.paid_left + total + (total*document.tax/100)
+    document.save
+  end
+
+  def substract_totals
+    document.sub_total = document.sub_total - total
+    document.tax_total = document.tax_total - (total*document.tax/100)
+    document.total = document.sub_total + document.tax_total
+    document.paid_left = document.paid_left - total - (total*document.tax/100)
+    document.save
+  end
 end
