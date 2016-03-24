641d0a2e5c4df964919f3cdccde8475cbcf3ed90
Papaya Labs <papayalabs@gmail.com>
2016-03-24 09:07:57 -0430

    Fix search javascript create and type in product movement

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,6 +5,7 @@ class Account < ActiveRecord::Base
 
   validates :code, presence: true
   validates :name, presence: true
+  validates :city, presence: true 
   
   belongs_to :company, :foreign_key => 'domain'
   
